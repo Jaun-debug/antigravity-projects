646e1f33bfd7b714fd5a2593dc6fad9368dbc7db
Jaun-debug <bookings@desert-tracks.com>
2026-06-03 17:15:42 +0200

    Add organised rate sheets folder with golden lodge rate sheet"

--- _Desert Tracks/dt_library/_Scripts & Generators/server.js
+++ _Desert Tracks/dt_library/_Scripts & Generators/server.js
@@ -43,7 +43,12 @@ const server = http.createServer((req, res) => {
         res.end(`Server Error: ${err.code}`);
       }
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
+      res.writeHead(200, { 
+        'Content-Type': contentType,
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      });
       res.end(content, 'utf-8');
     }
   });
